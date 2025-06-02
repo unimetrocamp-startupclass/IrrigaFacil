4bb09a0d07fc74939f374647921272ff8c3ab0c7
Soffy-bit <sofiamblima@gmail.com>
2025-06-02 17:11:06 -0300

    Update main.c
    Removing LCD since its not needed

--- main.c
+++ main.c
@@ -4,21 +4,12 @@ const int SensorUmi = A0;
 const int motor = 11;
 const int LedVerde = 12;
 
-LiquidCrystal LCD(7, 6, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
-  LCD.begin(16, 2);
-  LCD.print("Irriga");
-  LCD.setCursor(0, 1);
-  LCD.print("Facil!");
   pinMode(motor, OUTPUT);
   pinMode(LedVerde, OUTPUT);
   delay(2000);
-  LCD.clear();
-  LCD.print("Umidade = ");
-  LCD.setCursor(0, 1);
-  LCD.print("Irrigador = ");
 }
 
 void loop() {
@@ -27,15 +18,10 @@ void loop() {
   
   int UmidadePercentual = map(value, 1023, 0, 0, 100);
   
-  LCD.setCursor(11, 0);
-  LCD.print(UmidadePercentual);
-  LCD.print("% ");
-  LCD.setCursor(12, 1);
 
   if (UmidadePercentual < 40) { 
     digitalWrite(motor, HIGH);
     digitalWrite(LedVerde, HIGH);
-    LCD.print("ON ");
   } else {
     digitalWrite(motor, LOW);
     digitalWrite(LedVerde, LOW);
